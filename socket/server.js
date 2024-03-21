b90154b664a10db9ccc87fffe550832ad960c178
Dennis Audu <dennisaudu@nexa-air.home>
2024-03-21 00:25:39 -0400

    changes

--- socket/server.js
+++ socket/server.js
@@ -1,14 +1,13 @@
 import express from "express";
 import {SocketClient, sleep} from './socket.js'
-import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 app.use(express.urlencoded({ extended: true }))
 
 const MC_IP_ADDR = "127.0.0.1"; //D: pass NETWORK ip address
-const MC_PORT = 9910; //D: pass port number
-const socket = new SocketClient(MC_IP_ADDR, MC_PORT); //D: pass ip address and port
+const MC_PORT = 9910; //D: pass PORT number
+const socket = new SocketClient(MC_IP_ADDR, MC_PORT); //D: create new UDP socket client
 
 app.post('/streamData', async (req, res) => {
     try {
@@ -17,10 +16,11 @@ app.post('/streamData', async (req, res) => {
 
         //D: data is a key-value pair, so pass the value to the sendData function. 
         // I tested this with a key pair value of "value" and it worked.
-        
+
         if(data !== undefined){
             // send to the udp socket
             await socket.sendData(data.value);
+
             if (socket.packetsSent === 5) {
                 console.log('Sleeping for 5 seconds');
                 await sleep(5000); // Sleep for 5 seconds
@@ -32,7 +32,6 @@ app.post('/streamData', async (req, res) => {
             res.status(400).send('Data not found in request');
         }
         
-
     } catch (error) {
         console.error('Error processing request:', error);
         res.status(500).send('An error occurred while processing the request');
